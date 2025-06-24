82a63f3f0e92db53c5edfa7c7440cb929d9520ac
deetesh <gdeetesh@gmail.com>
2025-06-24 23:54:22 +0400

    add middleware, access middleware in route

--- app/Http/Middleware/AuthMiddleware.php
+++ app/Http/Middleware/AuthMiddleware.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Middleware;
 
+use App\Http\Controllers\AuthController;
 use Closure;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
@@ -17,7 +18,7 @@ class AuthMiddleware
     public function handle(Request $request, Closure $next): Response
     {
         if (!Auth::check()){
-            return redirect('/login'); 
+           return redirect('/login'); 
         }
 
         return $next($request);
